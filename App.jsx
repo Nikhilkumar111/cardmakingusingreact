44a7c2e3dd84a6d1264d304fd3218b962f543d6f
Nikhil kumar <145525419+Nikhilkumar111@users.noreply.github.com>
2024-09-05 12:21:07 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import"./App.css";
+// ********cardusingreact*****************
+import Producttab from "./Producttab.jsx";
+
+
+function App() {
+ return (
+ <div>
+    <h2>Block buster deals|| shop now</h2>
+<Producttab/>
+</div>
+ );
+}
+
+export default App
